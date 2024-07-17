d2d0ea130355790d6e637b33264becc48e576d40
ahmed helmiQA <ahmed.systemtester@gmail.com>
2024-07-18 00:30:18 +0300

    Screenshot

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,3 +1,4 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -9,6 +10,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.xml.sax.Locator;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,7 +21,7 @@ public class Selenium {
     public static WebDriver driver = new EdgeDriver();
     public static String firstTab;
     public static String secondTab;
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
       /*  OpenBroswer("https://the-internet.herokuapp.com/login");
         OpenBrowserUsingNavigation("https://www.nezamacademy.com");
         ClickOnBack();
@@ -43,9 +46,9 @@ public class Selenium {
         KeyUisingAction();
         UpperKeys();
         */
-        OpenBrowserUsingNavigation("https://the-internet.herokuapp.com/key_presses");
+        OpenBrowserUsingNavigation("https://the-internet.herokuapp.com/forgot_password");
         implicitWait();
-
+        takingScreenshot("screenbutton2");
       //QuiteWindows();
 
     }
@@ -207,6 +210,14 @@ public class Selenium {
     public static void scrollingUsingAction(){
         new Actions(driver).scrollToElement(byToWebelement(By.id(""))).perform();
         driver.findElement(By.id("")).sendKeys("test");
+    }
+    public static void takingScreenshot(String imagename) throws IOException {
+        String path = "E:\\Nezam\\Seleniumexercise\\src\\main\\resources\\";
+        //File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File src = (byToWebelement(By.cssSelector("button[class='radius']"))).getScreenshotAs(OutputType.FILE);
+        File target = new File(path+imagename+".png");
+        FileUtils.copyFile(src,target);
+
     }
 
     public static void CloseWendows(){
